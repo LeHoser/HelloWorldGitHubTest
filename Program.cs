12a3d89a0693cc0ccab40b345d5d64d726646436
LeHoser <nathan.lloyd.7003@gmail.com>
2022-09-12 10:58:25 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.SetWindowSize(15, 5);
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
